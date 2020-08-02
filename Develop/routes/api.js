606da824f76159045f5335ed45306bfc92129f99
calebwelch <caleb.welch11@gmail.com>
2020-08-02 13:03:19 -0400

    commented the app

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,32 +1,41 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+// requiring our model and a means to route
 
-router.post("/api/transaction", ({body}, res) => {
+// post --- route
+// we want mongoose data to be an object so we do it right in the
+// paramaters of the callback instead of doing req then req.body we can just use 'body'
+router.post("/api/transaction", ({ body }, res) => {
+  // mongoose create new document with data from body
   Transaction.create(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
+      // remember on err we want to send a proper server status!
       res.status(404).json(err);
     });
 });
-
-router.post("/api/transaction/bulk", ({body}, res) => {
+// looks like a post---route that we want if we are offline. When we are back online we can post all the transactions in the queue
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
-
+// get --- route for the history of transactions to display and chart data
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
+  // empty mongoose call finds all
+  Transaction.find({})
+    // sorts by first date
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
--- Develop/server.js
+++ Develop/server.js
@@ -2,22 +2,22 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+// initialize our app and call in mongoose
 const PORT = 3000;
 
 const app = express();
 
 app.use(logger("dev"));
-
+// middleware
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+// mongoose connection
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
